b9e3155a3fd6317cb8ce1475e63b3c549299cc55
s-phal <sam@samphal.com>
2023-12-14 06:49:23 -0600

    Add project files.

---
+++ HSFuncBuddy/Application/Config/DBConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace HSFuncBuddy.Application.Config
+{
+    public class DBConnection
+    {
+        public static string GetConnectionString()
+        {
+
+            return "Server=(localdb)\\MSSQLLocalDB;Database=funcbuddy;Trusted_Connection=True;";
+
+        }
+    }
+}
---
+++ HSFuncBuddy/Application/DataAccess/FuncBuddyDAO.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using HSFuncBuddy.Application.Config;
+using HSFuncBuddy.Application.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HSFuncBuddy.Application.DataAccess
+{
+    public class FuncBuddyDAO
+    {
+        public string connectionString { get; } = DBConnection.GetConnectionString();
+
+        public List<FuncBuddy> RetrieveAll()
+        {
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM func_buddy";
+
+                return connection.Query<FuncBuddy>(sqlQuery).ToList();
+
+            }
+        }
+
+        public void Create(FuncBuddy funcBuddy)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "" +
+                "INSERT INTO func_buddy " +
+                "(title, content, language_id) " +
+                "VALUES " +
+                "(@Title, @Content, 1) ";
+
+                connection.Execute(sqlQuery, new
+                {
+                    Title = funcBuddy.title,
+                    Content = funcBuddy.content
+                });
+            }
+        }
+
+    }
+
+}
